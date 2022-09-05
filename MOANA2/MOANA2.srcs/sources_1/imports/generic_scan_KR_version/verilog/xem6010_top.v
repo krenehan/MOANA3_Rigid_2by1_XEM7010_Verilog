9359d61d3ac8939668dba771861e514eea6a012d
Kevin Renehan <kr2743@columbia.edu>
2022-09-05 17:08:32 -0400

    Added MIG block and RAM ports

--- MOANA2/MOANA2.srcs/sources_1/imports/generic_scan_KR_version/verilog/xem6010_top.v
+++ MOANA2/MOANA2.srcs/sources_1/imports/generic_scan_KR_version/verilog/xem6010_top.v
@@ -34,23 +34,23 @@ module xem6010_top(
 	output		wire		[7:0]			led,
 	
 	// User signals
-	inout		wire		[25:0]		MC1
+	inout		wire		[25:0]		MC1,
 	
 	// DRAM connections
-//	inout  		wire 	[15:0] 		ddr3_dq,
-//	output 		wire 	[14:0] 		ddr3_addr,
-//	output 		wire 	[2:0]  		ddr3_ba,
-//	output 		wire          			ddr3_ras_n,
-//	output 		wire          			ddr3_cas_n,
-//	output 		wire          			ddr3_we_n,
-//	output 		wire          			ddr3_odt,
-//	output 		wire          			ddr3_cke,
-//	output 		wire 	[1:0]  		ddr3_dm,
-//	inout  		wire 	[1:0]  		ddr3_dqs_p,
-//	inout  		wire 	[1:0]  		ddr3_dqs_n,
-//	output 		wire          			ddr3_ck_p,
-//	output 		wire          			ddr3_ck_n,
-//	output 		wire          			ddr3_reset_n
+	inout  		wire 	[15:0] 		ddr3_dq,
+	output 		wire 	[14:0] 		ddr3_addr,
+	output 		wire 	[2:0]  		ddr3_ba,
+	output 		wire          			ddr3_ras_n,
+	output 		wire          			ddr3_cas_n,
+	output 		wire          			ddr3_we_n,
+	output 		wire          			ddr3_odt,
+	output 		wire          			ddr3_cke,
+	output 		wire 	[1:0]  		ddr3_dm,
+	inout  		wire 	[1:0]  		ddr3_dqs_p,
+	inout  		wire 	[1:0]  		ddr3_dqs_n,
+	output 		wire          			ddr3_ck_p,
+	output 		wire          			ddr3_ck_n,
+	output 		wire          			ddr3_reset_n
 	
 	
     );
@@ -328,6 +328,25 @@ module xem6010_top(
 	
 	
 	//------------------------------------------------------------------------
+	// DRAM signals
+	//------------------------------------------------------------------------
+	wire [28:0] app_addr;
+	wire [2:0] app_cmd;
+	wire app_en;
+	wire [127:0] app_wdf_data;
+	wire app_wdf_end;
+	wire app_wdf_wren;
+	wire [127:0] app_rd_data;
+	wire app_rd_data_end;
+	wire app_rd_data_valid;
+	wire app_rdy;
+	wire app_wdf_rdy;
+	wire [15:0] app_wdf_mask;
+//	wire dram_clk;
+	wire dram_rst;
+	wire init_calib_complete;
+	
+	//------------------------------------------------------------------------
 	// Debug signals
 	//------------------------------------------------------------------------
 	reg																		verify_refclk_running;
@@ -339,11 +358,11 @@ module xem6010_top(
 	//------------------------------------------------------------------------
 	// System clock differential to single-ended buffer
 	//------------------------------------------------------------------------
-    IBUFDS clock_buff (
-        .O(sys_clk), //buffer output
-        .I(sys_clk_p), //buffer input 
-        .IB(sys_clk_n) //  
-    );
+//    IBUFDS clock_buff (
+//        .O(sys_clk), //buffer output
+//        .I(sys_clk_p), //buffer input 
+//        .IB(sys_clk_n) //  
+//    );
 
 
 assign ref_clk_mmcm_muxed = clk_select[0] ? ref_clk_mmcm[0] : ref_clk_mmcm[1];
@@ -842,6 +861,59 @@ BUFGCTRL_ref_clk_mmcm_muxed_inst (
 			
         end
     endgenerate
+    
+    
+    //-------------------------------------------------------------------------------
+    //  DDR3 MIG Module
+    //-------------------------------------------------------------------------------
+    ddr3_256_16 u_ddr3_256_16 (
+
+		// Memory interface ports to DRAM
+		.ddr3_addr                      (ddr3_addr),  			// output [14:0]        ddr3_addr
+		.ddr3_ba                        (ddr3_ba),  			// output [2:0]        	ddr3_ba
+		.ddr3_cas_n                     (ddr3_cas_n),  			// output            	ddr3_cas_n
+		.ddr3_ck_n                      (ddr3_ck_n),  			// output [0:0]        	ddr3_ck_n
+		.ddr3_ck_p                      (ddr3_ck_p),  			// output [0:0]        	ddr3_ck_p
+		.ddr3_cke                       (ddr3_cke),  			// output [0:0]        	ddr3_cke
+		.ddr3_ras_n                     (ddr3_ras_n),  			// output            	ddr3_ras_n
+		.ddr3_reset_n                   (ddr3_reset_n),  		// output            	ddr3_reset_n
+		.ddr3_we_n                      (ddr3_we_n),  			// output            	ddr3_we_n
+		.ddr3_dq                        (ddr3_dq),  			// inout [15:0]        	ddr3_dq
+		.ddr3_dqs_n                     (ddr3_dqs_n),  			// inout [1:0]        	ddr3_dqs_n
+		.ddr3_dqs_p                     (ddr3_dqs_p),  			// inout [1:0]        	ddr3_dqs_p
+		.ddr3_dm                        (ddr3_dm),  			// output [1:0]        	ddr3_dm
+		.ddr3_odt                       (ddr3_odt),  			// output [0:0]        	ddr3_odt
+		
+		.init_calib_complete            (init_calib_complete),  // output            	init_calib_complete
+		
+		// Application interface ports
+		.app_addr                       (app_addr),  			// input [28:0]        	app_addr
+		.app_cmd                        (app_cmd),  			// input [2:0]        	app_cmd
+		.app_en                         (app_en),  				// input                app_en
+		.app_wdf_data                   (app_wdf_data),  		// input [127:0]        app_wdf_data
+		.app_wdf_end                    (app_wdf_end),  		// input                app_wdf_end
+		.app_wdf_wren                   (app_wdf_wren),  		// input                app_wdf_wren
+		.app_rd_data                    (app_rd_data),  		// output [127:0]    	app_rd_data
+		.app_rd_data_end                (app_rd_data_end),  	// output            	app_rd_data_end
+		.app_rd_data_valid              (app_rd_data_valid),  	// output            	app_rd_data_valid
+		.app_rdy                        (app_rdy),  			// output            	app_rdy
+		.app_wdf_rdy                    (app_wdf_rdy),  		// output            	app_wdf_rdy
+		.app_sr_req                     (1'b0),  				// input            	app_sr_req
+		.app_ref_req                    (1'b0),  				// input            	app_ref_req
+		.app_zq_req                     (1'b0),  				// input            	app_zq_req
+		.app_sr_active                  (),  					// output            	app_sr_active
+		.app_ref_ack                    (),  					// output            	app_ref_ack
+		.app_zq_ack                     (),  					// output            	app_zq_ack
+		.ui_clk                         (sys_clk),  			// output            	ui_clk
+		.ui_clk_sync_rst                (),  					// output            	ui_clk_sync_rst
+		.app_wdf_mask                   (app_wdf_mask),  		// input [15:0]        	app_wdf_mask
+		
+		// System Clock Ports
+		.sys_clk_p                      (sys_clk_p),  			// input                sys_clk_p
+		.sys_clk_n                      (sys_clk_n),  			// input                sys_clk_n
+		.sys_rst                        (dram_rst) 				// input 				sys_rst
+);
+    
 	
 	//-------------------------------------------------------------------------------
     //  Master Data Pipe Out Control
