aeb7fa07f7e2352b28d15c3a24d15fec4523edd5
Kevin Renehan <kr2743@columbia.edu>
2022-09-12 12:38:24 -0400

    created up_down_counter

---
+++ MOANA2/MOANA2.srcs/sources_1/imports/ram/up_down_counter.v
@@ -0,0 +1,54 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    15:54:29 01/06/2022 
+// Design Name: 
+// Module Name:    up_down_counter 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: Up down counter with no overflow
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module up_down_counter 
+	#( 	parameter WIDTH = 10)
+	(
+		input  wire clk,
+		input  wire rst,
+		input  wire inc,
+		input  wire dec,
+		output wire full,
+		output wire empty,
+		output reg [WIDTH-1:0] count
+    );
+	 
+	// Full and empty flags 
+	assign full = &count;
+	assign empty = !(|count);
+	
+	// Up down counter with no overflow
+	always @(posedge clk or posedge rst) begin
+		if (rst) begin
+			count <= 0;
+		end else if (inc & dec) begin
+			count <= count;
+		end else if (inc & ~full) begin
+			count <= count + 1;
+		end else if (dec & ~empty) begin
+			count <= count - 1;
+		end else begin
+			count <= count;
+		end
+	end
+			
+
+
+endmodule
